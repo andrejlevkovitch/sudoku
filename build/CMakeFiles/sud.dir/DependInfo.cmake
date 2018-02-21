ff03294f47e9773b001cac9258e3efc97ab2f501
andrejlevkovitch <andrejlevkivitch@gmail.com>
2018-02-21 15:23:32 +0300

    обновление структуры кода, использование cmake

---
+++ build/CMakeFiles/sud.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/andrej/Public/git/sudoku/sources/libsud/coincidence.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/coincidence.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/colors.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/colors.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/cursor.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/cursor.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/decision.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/decision.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/flags.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/flags.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/forecasting.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/forecasting.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/frame.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/frame.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/init.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/init.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/input_psbl_sc.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/input_psbl_sc.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/input_type.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/input_type.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/io_system.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/io_system.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/message.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/message.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/num_of_cros.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/num_of_cros.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/psbl_sc.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/psbl_sc.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/rand_array_special.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/rand_array_special.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/revers.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/revers.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/substitution.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/substitution.c.o"
+  "/home/andrej/Public/git/sudoku/sources/libsud/sud_maker.c" "/home/andrej/Public/git/sudoku/build/CMakeFiles/sud.dir/sources/libsud/sud_maker.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku.dir/sources/main.c.o"
+  "sudoku.pdb"
+  "sudoku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
